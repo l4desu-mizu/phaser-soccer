8a9ebbc4702116ec392c229e2148865a4d03a1fc
Norbert Dzikowski <norbert@perun.network>
2021-09-18 00:56:19 +0200

    webpack: Fix configuration for hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,30 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './out/index.js',
+  entry: './src/index.ts',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  devServer: {
+    static: "./dist",
+    devMiddleware: {
+      index: true,
+      writeToDisk: true,
+    },
+  },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   target: 'web',
   plugins: [
